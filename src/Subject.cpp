35b1a44aa7fb8f61752dc24158c2adfdafdb0d42
DanielAnd0 <danxd7952@gmail.com>
2026-02-02 05:10:41 +0200

    Milestone 3 - adaugare clasa de Factory, utilizare observer design pattern, clasa template pentru tratarea coliziunilor

--- src/Subject.cpp
+++ src/Subject.cpp
@@ -7,7 +7,13 @@
 
 void Subject::Attach (Observer* o) {_observers.push_back(o); }
 void Subject::Detach (Observer* o) {
-    for (auto i = _observers.begin(); i < _observers.end(); ++i) {if (*i == o) _observers.erase(i); break;}
+    for (auto it = _observers.begin(); it != _observers.end(); )
+    {
+        if (*it == o)
+            it = _observers.erase(it);
+        else
+            ++it;
+    }
 }
 void Subject::Notify(){
     for (auto & _observer : _observers) {
